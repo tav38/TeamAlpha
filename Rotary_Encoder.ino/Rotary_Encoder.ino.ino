96c19e5dd369ca84e83707c08cd079beac273e8b
Lauren Gilfillan <lag179@pitt.edu>
2024-02-27 11:59:21 -0500

    adjusted rotary encode

--- Rotary_Encoder.ino/Rotary_Encoder.ino.ino
+++ Rotary_Encoder.ino/Rotary_Encoder.ino.ino
@@ -23,8 +23,12 @@
 #include <Arduino.h>
 #include <RotaryEncoder.h>
 
+// Example for Arduino UNO with input signals on pin 2 and 3
 #define PIN_IN1 A2
 #define PIN_IN2 A3
+
+
+
 
 // Setup a RotaryEncoder with 4 steps per latch for the 2 signal input pins:
 // RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::FOUR3);
@@ -34,26 +38,25 @@ RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::TWO03);
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
   while (! Serial);
   Serial.println("SimplePollRotator example for the RotaryEncoder library.");
 } // setup()
 
+
 // Read the current position of the encoder and print out when changed.
 void loop()
 {
-  //number_we_want_to_obtain
   static int pos = 0;
   encoder.tick();
 
-
-  //wait for some amount of time so they have time to move
   int newPos = encoder.getPosition();
   if (pos != newPos) {
-    //exit with failure
-  }
-  else if(pos == newPos){
-    //sucessfull attempt 
+    Serial.print("pos:");
+    Serial.print(newPos);
+    Serial.print(" dir:");
+    Serial.println((int)(encoder.getDirection()));
+    pos = newPos;
   } // if
 } // loop ()
 
