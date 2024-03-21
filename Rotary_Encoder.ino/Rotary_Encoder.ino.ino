28bf742638a1331a3f43620466869042489fb28e
tav38 <122564985+tav38@users.noreply.github.com>
2024-03-20 20:27:21 -0400

    Update Rotary_Encoder.ino.ino

--- Rotary_Encoder.ino/Rotary_Encoder.ino.ino
+++ Rotary_Encoder.ino/Rotary_Encoder.ino.ino
@@ -1,60 +1,61 @@
-// -----
-// SimplePollRotator.ino - Example for the RotaryEncoder library.
-// This class is implemented for use with the Arduino environment.
-//
-// Copyright (c) by Matthias Hertel, http://www.mathertel.de
-// This work is licensed under a BSD 3-Clause License. See http://www.mathertel.de/License.aspx
-// More information on: http://www.mathertel.de/Arduino
-// -----
-// 18.01.2014 created by Matthias Hertel
-// 04.02.2021 conditions and settings added for ESP8266
-// -----
-
-// This example checks the state of the rotary encoder in the loop() function.
-// The current position and direction is printed on output when changed.
-
-// Hardware setup:
-// Attach a rotary encoder with output pins to
-// * A2 and A3 on Arduino UNO.
-// * D5 and D6 on ESP8266 board (e.g. NodeMCU).
-// Swap the pins when direction is detected wrong.
-// The common contact should be attached to ground.
-
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 #include <Arduino.h>
 #include <RotaryEncoder.h>
 
-#define PIN_IN1 A2
-#define PIN_IN2 A3
+#define Rotary_A A2
+#define Rotary_B A3
 
-// Setup a RotaryEncoder with 4 steps per latch for the 2 signal input pins:
-// RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::FOUR3);
+static int rot_pos;
+static int sucess = 0;
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+RotaryEncoder encoder(Rotary_A,Rotary_B, RotaryEncoder::LatchMode::FOUR3);
 
-// Setup a RotaryEncoder with 2 steps per latch for the 2 signal input pins:
-RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::TWO03);
+void setup() {
+  lcd.begin(16,2);
+  lcd.clear();
+  rot_pos = 0;
+}
 
-void setup()
-{
-  Serial.begin(115200);
-  while (! Serial);
-  Serial.println("SimplePollRotator example for the RotaryEncoder library.");
-} // setup()
-
-// Read the current position of the encoder and print out when changed.
-void loop()
-{
-  //number_we_want_to_obtain
-  static int pos = 0;
-  encoder.tick();
-
-
-  //wait for some amount of time so they have time to move
-  int newPos = encoder.getPosition();
-  if (pos != newPos) {
-    //exit with failure
-  }
-  else if(pos == newPos){
-    //sucessfull attempt 
-  } // if
-} // loop ()
-
-// The End
+void loop() {
+  sucess = 0;
+    lcd.clear();
+    int rot_code = int(random(0,100));
+    String rotcodes = String(rot_code);
+    String placeholder = "Combo 1 : " + rotcodes;
+    lcd.print(placeholder);
+    delay(1000);
+unsigned long startTime = millis();
+int temp_rot = 0;
+while (millis() - startTime < 10000) { // 10-second timeout
+      encoder.tick();
+      int rot_pos = encoder.getPosition();
+      if(rot_pos == rot_code){
+        lcd.clear();
+        lcd.print("Code Cracked");
+        delay(5000);
+        sucess = 1;
+        break;
+      }
+      else if(temp_rot != rot_pos) {
+      lcd.clear();   
+      lcd.print("pos:");
+      String poss = String(rot_pos); 
+      lcd.print(poss);
+      temp_rot = rot_pos;
+      }
+      }
+    
+  
+    if(rot_pos == rot_code){
+      lcd.clear();
+      lcd.print("Code Cracked");
+      delay(10000);
+    }
+    
+    if(sucess == 0){
+      lcd.clear();
+      lcd.print("Womp Womp");
+      delay(5000);
+    }
+}
