7a8cfb018517dc3513b7e34762cbf09a1d2d5a33
tav38 <122564985+tav38@users.noreply.github.com>
2024-03-25 20:25:35 -0400

    Update Speaker_Keypad_Code.ino

--- Speaker_Keypad_Code/Speaker_Keypad_Code.ino
+++ Speaker_Keypad_Code/Speaker_Keypad_Code.ino
@@ -8,13 +8,18 @@
 || #
 */
 #include <Keypad.h>
-#define KeyPad_OUT D1
-#define KeyPad_In D2
+#define KeyPad_OUT 10
+#define KeyPad_In 11
+#include <DFRobotDFPlayerMini.h>
+#include "SoftwareSerial.h"
+
+SoftwareSerial mySoftwareSerial(7, 6); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
 
 
 
 const byte ROWS = 4; //four rows
-const byte COLS = 4; //three columns
+const byte COLS = 4; //four columns
 
 
 
@@ -24,19 +29,20 @@ char keys[ROWS][COLS] = {
   {'7','8','9','C'},
   {'*','0','#','D'}
 };
-byte rowPins[ROWS] = {3,2,1,0};
+byte rowPins[ROWS] = {9,8,7,6};
  //connect to the row pinouts of the keypad
-byte colPins[COLS] = {7,6,5,4}; //connect to the column pinouts of the keypad
+byte colPins[COLS] = {5,4,3,2}; //connect to the column pinouts of the keypad
 
-char code1 ={'1','4','7','*' }
-char code2 ={'0','2','5','8' }
-char code3 ={'9','#','3','6' }
-char code4 ={'2','B','D','A' }
-char code5 ={'4','3','3','1' }
+char code1[4] ={'1','4','7','*' };
+char code2[4] ={'0','2','5','8' };
+char code3[4] ={'9','#','3','6' };
+char code4[4] ={'2','B','D','A' };
+char code5[4] ={'4','3','3','1' };
 int Game_Status = 0;
 int Win_Status = 0;
 int index = 0;
 int action;
+int codenum = 0;
 
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
@@ -44,68 +50,81 @@ Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 void setup(){
 pinMode(KeyPad_OUT,OUTPUT);
 pinMode(KeyPad_In,INPUT);
-int codenum = 0;
 keypad.addEventListener(keypadEvent);
-
+mySoftwareSerial.begin(9600);
+myDFPlayer.begin(mySoftwareSerial);
+myDFPlayer.setTimeOut(5000);
+delay(200);
+myDFPlayer.volume(30);  //Set volume value. From 0 to 30
+delay(200);
 }
   
 void loop(){
   
   if(KeyPad_In == 1){
-    action = rand(0,2);
-    codenum =rand(0,4);
+    action = random(0,2);
+    codenum =random(1,5);
     pinMode(KeyPad_In,OUTPUT);
     digitalWrite(KeyPad_In,1);
     Game_Status = 1;
+    int num = 0;
+     myDFPlayer.play(codenum);
+
     //speaker output
-    while(Game_Staus == 1){
+    while(Game_Status == 1){
       //wait for input
-      char key = keypad.getKey();
-    }
-    digitWrite(KeyPad_Out,Win_Status);
-
-    
-
-
+      if (index >= 3){
+        Game_Status == 0;
+      }
+      else{
+      char key = keypad.getKey();}
+      }
+    digitalWrite(KeyPad_OUT,Win_Status);
+    digitalWrite(KeyPad_In,0);
+    pinMode(KeyPad_In,INPUT);
   }
 }
 
 // Taking care of some special events.
 void keypadEvent(KeypadEvent key){
-    switch codenum {
-      case 0:
-        if(key == code1[index]){
-          Game_Status == 1;
-          index++;
-        }
-        else{Game_Status == 0;}
+    switch (codenum) {
       case 1:
-        if(key == code2[index]){
-          Game_Status == 1;
+        if(key == code1[index]){
+          Win_Status == 1;
           index++;
         }
-        else{Game_Status == 0;}
+        else{Win_Status == 0;
+        Game_Status == 0;}
       case 2:
-        if(key == code3[index]){
-          Game_Status == 1;
+        if(key == code2[index]){
+          Win_Status == 1;
           index++;
         }
-        else{Game_Status == 0;}
-
+        else{Win_Status == 0;
+        Game_Status == 0;}
       case 3:
+        if(key == code3[index]){
+          Win_Status == 1;
+          index++;
+        }
+        else{Win_Status == 0;
+        Game_Status == 0;}
+
+      case 4:
         if(key == code4[index]){
           Game_Status == 1;
           index++;
         }
-        else{Game_Status == 0;}
+        else{Win_Status == 0;
+        Game_Status == 0;}
 
-      case 4:
+      case 5:
         if(key == code5[index]){
-          Game_Status == 1;
+          Win_Status == 1;
           index++;
         }
-        else{Game_Status == 0;}
+        else{Win_Status == 0;
+        Game_Status == 0;}
     }
  
     }
-}
